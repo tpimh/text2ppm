d26732c460a723dbe6205722c0ec34033055e47f
Dmitry Golovin <dima@golovin.in>
2013-06-21 17:53:16 +0400

    now full string renders

--- text2ppm.c
+++ text2ppm.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <font8x8_basic.h>
 
+#define CHAR_H 8
+#define CHAR_W 8
+
 int main() {
-	int h = 640;
-	int w = 480;
 
-	printf("P6\n");			// file format
-	printf("%d %d\n", h, w);	// height and width
-	printf("1\n");			// color depth
+        char str[] = "This is the text TO RENDER: ^@#&*~~adfFDK3481029";
+	int str_l = sizeof(str) - 1;
 
-	char *bitmap = font8x8_basic[65];
+	int s[CHAR_H][CHAR_W * str_l];
 
-	int s[8][8];
-	for (int x = 0; x < 8; x++) {
-		for (int y = 0; y < 8; y++) {
-			int set = bitmap[x] & 1 << y;
-			s[x][y] = set ? 1 : 0;
-		}
+	for (int i = 0; i < str_l; i++) {
+		char *bitmap = font8x8_basic[str[i]];
+
+        	for (int x = 0; x < 8; x++) {
+                	for (int y = 0; y < 8; y++) {
+                        	int set = bitmap[x] & 1 << y;
+                        	s[x][y + CHAR_W * i] = set ? 1 : 0;
+                	}
+        	}
 	}
 
+	int h = CHAR_H;
+	int w = CHAR_W * str_l;
 
-	for (int k = 0; k < w; k++) {
-		for (int i = 0; i < h; i++) {
-			if (i > 19 && i < 28 && k > 19 && k < 28) {
-				if (s[k-20][i-20] == 1){
-					printf("%c%c%c", 0, 0, 0);
-				} else {
-					printf("%c%c%c", 1, 1, 1);
-				}
-			} else { printf("%c%c%c", 1, 1, 1); }
+	printf("P6\n");			// file format
+	printf("%d %d\n", w, h);	// width and height
+	printf("1\n");			// color depth
+	
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			if (s[y][x] == 1) {
+				printf("%c%c%c", 0, 0, 0);
+			} else {
+				printf("%c%c%c", 1, 1, 1);
+			}
 		}
 	}
 
