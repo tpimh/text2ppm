3947f74ffdf7a85c01f56be946dc722da4a77092
Dmitry Golovin <dima@golovin.in>
2013-06-21 13:59:04 +0400

    simple pattern generator

--- text2ppm.c
+++ text2ppm.c
@@ -6,11 +6,15 @@ int main() {
 
 	printf("P6\n");			// file format
 	printf("%d %d\n", h, w);	// height and width
-	printf("255\n");		// color depth
+	printf("1\n");			// color depth
 
 	for (int k = 0; k < w; k++) {
 		for (int i = 0; i < h; i++) {
-			printf("%c%c%c", 0, 0, 0);
+			if (i * k % 5 == 0) {
+				printf("%c%c%c", 0, 0, 0);
+			} else {
+				printf("%c%c%c", 1, 1, 1);
+			}
 		}
 	}
 
